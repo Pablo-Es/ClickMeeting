af65a7ab9427dadb8fb2c144b1b3de4d7dc69df4
Pablo-Es <szyposz@gmail.com>
2018-01-19 22:49:48 +0100

    added slider

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,34 @@
 $(function() {
-    var body = $("html, body");
-    var button = $('.scroll-top');
+    const body = $("html, body");
+    const button = $('.scroll-top');
+    const $slider = $('.slider__main-content');
+    const $slides = $slider.find('.slider__slide');
+    let $currSlide = $slides.first();
+    const $controlArrows = $slider.find('.slider__controls');
+    $slides.hide();
+    $currSlide.show();
+
+    function slideOnClick() {
+        const $this = $(this);
+        if($this.hasClass('slider__control-ico-left')){
+            var $nextSlide = $currSlide.prev('.slider__slide');
+            if($nextSlide.size() < 1){
+                $nextSlide = $slides.last();
+            }
+        }else{
+            $nextSlide = $currSlide.next('.slider__slide');
+            if($nextSlide.size() < 1){
+                $nextSlide = $slides.first();
+            }
+        }
+        $currSlide.fadeOut('medium');
+        $nextSlide.fadeIn('medium');
+        $currSlide = $nextSlide;
+    }
+    $controlArrows.on('click', slideOnClick);
+
     $(window).scroll(function() {
-        var topPos = $(this).scrollTop();
+        let topPos = $(this).scrollTop();
         if (topPos > 100) {
             button.removeClass('hidden');
         } else {
